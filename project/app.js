42285cab74a092d2bebed6b527091cba7bbb10ad
DJ Bostock <douglasjbostock@gmail.com>
2025-11-03 14:54:42 -0600

    test routing requests

--- project/app.js
+++ project/app.js
@@ -1,12 +1,28 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<body><h1>Test response</h1></body>');
-    res.write('</html>');
-    res.end();
+    const url = req.url;
+
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Enter A Message</title></head>');
+        res.write('<body>');
+        res.write('<form action="/message" method="POST">');
+        res.write('<input type="text" name="message">');
+        res.write('<button type="submit">Send</button>');
+        res.write('</form>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    } else {
+        console.log(req.url, req.method, req.headers);
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<body><h1>Test response</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
 });
 
 server.listen(80);
