d1e39ce561d5ca632778e7e97eae713c233d6902
DJ Bostock <douglasjbostock@gmail.com>
2025-11-05 12:00:00 -0600

    cumulative updates

--- project/app.js
+++ project/app.js
@@ -1,44 +1,44 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
+  const url = req.url;
+  const method = req.method;
 
-    if (url === '/') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Enter A Message</title></head>');
-        res.write('<body>');
-        res.write('<form action="/message" method="POST">');
-        res.write('<input type="text" name="message">');
-        res.write('<button type="submit">Send</button>');
-        res.write('</form>');
-        res.write('</body>');
-        res.write('</html>');
-        res.end();
-    } else if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        res.end();
-    } else {
-        console.log(req.url, req.method, req.headers);
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<body><h1>Test response</h1></body>');
-        res.write('</html>');
-        res.end();
-    }
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter A Message</title></head>");
+    res.write("<body>");
+    res.write('<form action="/message" method="POST">');
+    res.write('<input type="text" name="message">');
+    res.write('<button type="submit">Send</button>');
+    res.write("</form>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      // console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    res.end();
+  } else {
+    console.log(req.url, req.method, req.headers);
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body><h1>Test response</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
 });
 
 server.listen(80);
